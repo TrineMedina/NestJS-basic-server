a453923a2a7d17eebb788b41e16c4ee9e1b8996e
TrineMedina <trinefirestone@gmail.com>
2022-11-05 16:48:52 -0400

    updated gitignore

--- src/prisma/prisma.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common';
-import { PrismaService } from './prisma.service';
-
-@Global() // This makes this module available in all modules
-@Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
-})
-export class PrismaModule {}
